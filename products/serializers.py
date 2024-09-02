cd5ab38f1fb073d782e12b4872c1b6a673cfc643
Auwais <115638720+0xAuwais@users.noreply.github.com>
2024-09-02 20:09:58 +0300

    Delete products directory

--- products/serializers.py
+++
@@ -1,9 +0,0 @@
-# products/serializers.py
-
-from rest_framework import serializers
-from .models import Product
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
