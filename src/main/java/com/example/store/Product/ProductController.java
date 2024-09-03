53737adff78be4bcc09a15465887d63b0cb2833c
Obaidat <ahmadaymanobaidat@gmail.com>
2024-09-03 16:33:48 +0300

    Ahmad Obaidat

---
+++ src/main/java/com/example/store/Product/ProductController.java
@@ -0,0 +1,44 @@
+package com.example.store.Product;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+// Api layer
+
+@RestController
+public class ProductController {
+
+    ProductService productService;
+
+    @Autowired
+    public ProductController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return this.productService.save(product);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return this.productService.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        return this.productService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        return this.productService.update(id, product);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProductById(@PathVariable Long id) {
+        return this.productService.delete(id);
+    }
+}
